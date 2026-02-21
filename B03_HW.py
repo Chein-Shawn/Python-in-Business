57f4d17b53ca20433b13aa8cf66461c23c992e9f
Chein-Shawn <shanewn931131@gmail.com>
2026-02-21 16:27:07 +0800

    Update

--- B03_HW.py
+++ B03_HW.py
@@ -1,7 +1,7 @@
 fpath = "/Users/shawn/Desktop/GitHub/Python-in-Business/risk.txt"
 with open(fpath, 'r', encoding='utf-8') as file:
-    lines = []
-    for line in file:
-        lines.append((line.strip()).split())
-    print(lines)
-        
+    lines = file.readlines()
+
+n, m = map(int, lines[0].split())
+
+    

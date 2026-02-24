83a6c3ffca865133f89b129bd31f1c94eb29e3a6
Chein-Shawn <shanewn931131@gmail.com>
2026-02-24 13:18:45 +0800

    update

--- C03_HW.py
+++ C03_HW.py
@@ -1,20 +1,21 @@
-def CournotEq(a, c1, c2, n):
+def CournotEq(a, b, c1, c2, n):
     # iteration 0: entering the market
     q1 = list()
-    q1.append((a - c1) / 2)
+    q1.append((a + c1) / 2)
     q2 = list()
-    q2.append(max((a - q1[0] - c2) / 2, 0))
+    q2.append(max((a + b * q1[0] + c2) / 2, 0))
 
     # in each iteration, respond once
     for i in range(n):
-        q1Next = max((a - q2[i] - c1) / 2, 0)
+        q1Next = max((a + b * q2[i] + c1) / 2, 0)
         q1.append(q1Next)
-        q2Next = max((a - q1Next - c2) / 2, 0)
+        q2Next = max((a + b * q1Next + c2) / 2, 0)
         q2.append(q2Next)
     return q1, q2
 
-a = 10.0
-c1 = 1.0
-c2 = 3.0
-n = 10
-q1, q2 = CournotEq(a, c1, c2, n)
+a, b, c1, c2, n = input().strip().split(",")
+a, b, c1, c2 = float(a), float(b), float(c1), float(c2)
+n = int(n)
+q1, q2 = CournotEq(a, b, c1, c2, n)
+
+print("%0.2f %0.2f" %(q1[n], q2[n]))
